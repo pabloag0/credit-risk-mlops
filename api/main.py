8517eb50f65a3c932476c1302baf6429af233f90
pabloag0 <pablag02@ucm.es>
2026-08-08 19:20:55 +0200

    Static FrontEnd

--- api/main.py
+++ api/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from api.schemas import LoanApplication, PredictionResponse
 from api.model_loader import predict
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Credit Risk API", version="1.0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
